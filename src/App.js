0897cb07eac1d5df06a3128aba8241bf8431d504
Ezequiel Alvarez <ezenigm@hotmail.com>
2022-07-24 18:43:34 -0300

    Counter and Search

--- src/App.js
+++ src/App.js
@@ -7,20 +7,49 @@ import {CreateTodoButton} from './CreateTodoButton';
 
 // import './App.css';
 
-const todos = [
+const defaultTodos = [
   {text:'cortar cebolla', completed: true},
   {text:'comprar pan', completed: false},
   {text:'terminar la tarea', completed: false}
 ]
 
-function App(props) {
+function App() {
+  //estado inicial de nuestros TODOs
+  const [todos,setTodos] = React.useState(defaultTodos); 
+   //cantidad de TODOs completados
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  //cantidad total de TODOs
+  const totalTodos = todos.length;
+
+  //estado de nuestra busqueda
+  const [searchValue,setSearchValue] = React.useState('');
+  let searchedTodos = [];
+  if(!searchValue.length >=1){
+    searchedTodos = todos
+  }else{
+    searchedTodos = todos.filter(todo => {
+            const todoText = todo.text.toLowerCase();
+            const searchText = searchValue.toLowerCase();
+            return todoText.includes(searchText);
+    })
+
+  }
+
+
   return (
     <React.Fragment>
         
-        <TodoCounter/>
-        <TodoSearch/>
+        <TodoCounter
+          total = {totalTodos}
+          completed = {completedTodos}
+        />
+
+        <TodoSearch
+          searchValue = {searchValue}
+          setSearchValue = {setSearchValue}
+        />
         <TodoList>
-          {todos.map(todo => (
+          {searchedTodos.map(todo => (
             <TodoItem 
                 key={todo.text} 
                 text={todo.text} 
