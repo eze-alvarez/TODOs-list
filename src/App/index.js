2de1bfcb78f273060d03786e86e39334895f8a36
Ezequiel Alvarez <ezenigm@hotmail.com>
2022-07-25 18:42:32 -0300

    Custom Hook for Local Storage

--- src/App/index.js
+++ src/App/index.js
@@ -8,21 +8,40 @@ import { AppUI } from "./appUI";
 //   {text:'terminar la tarea', completed: false}
 // ]
 
+function useLocalStorage (itemName, initialValue){
+    //traemos nuestros TODOs almacenados
+    const localStorageItem = localStorage.getItem(itemName);
+    let parsedItem;
+
+    if(!localStorageItem){
+  //si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos un arrav vacio []
+          localStorage.setItem(itemName,JSON.stringify(initialValue));
+          parsedItem = initialValue;
+    } else {  //si existen TODOs en el localStorage los regresamos como nuestros todos
+          parsedItem = JSON.parse(localStorageItem)
+    }
+
+    //guardamos nuestros TODOs del localStorage en nuestro estado
+    const [item,setItem] = React.useState(parsedItem); 
+
+    // se crea la funcion para actualizar nuestro localStorage
+    const saveItem = (newItem) => {
+          const stringifiedItem = JSON.stringify(newItem);// convertimos a string
+          localStorage.setItem(itemName,stringifiedItem);// los guardamos
+          setItem(newItem); //actualizamos el estado de nuestros TODOs
+  }; 
+
+  return [
+    item, //recibir lo que quiera guarfar en localStorage
+    saveItem //actualizar lo que quiera guarfar en localStorage
+  ];
+}
+
 function App() {
-//traemos nuestros TODOs almacenados
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  let parsedTodos;
+  
+  const [todos,saveTodos] = useLocalStorage('TODOS_V1',[]); //cuando se creo el custom Hook
 
-  if(!localStorageTodos){
-//si el usuario es nuevo no existe un item en localStorage, por lo tanto guardamos un arrav vacio []
-    localStorage.setItem('TODOS_V1',JSON.stringify([]));
-    parsedTodos = [];
-  } else {  //si existen TODOs en el localStorage los regresamos como nuestros todos
-    parsedTodos = JSON.parse(localStorageTodos)
-  }
-
-  //guardamos nuestros TODOs del localStorage en nuestro estado
-  const [todos,setTodos] = React.useState(parsedTodos); 
+  
    //cantidad de TODOs completados
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   //cantidad total de TODOs
@@ -41,12 +60,7 @@ function App() {
     }); 
   };
 
-// se crea la funcion para actualizar nuestro localStorage
-  const saveTodos = (newTodos) => {
-    const stringifiedTodos = JSON.stringify(newTodos);// convertimos a string
-    localStorage.setItem('TODOS_V1',stringifiedTodos);// los guardamos
-    setTodos(newTodos); //actualizamos el estado de nuestros TODOs
-}; 
+
   
 //completando TODO
   const completeTodo = (text) =>{
